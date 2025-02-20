02e72d0650c6d605f94bff529b591d421210e437
Ciro Sirignano <cirsir@posteo.net>
2025-02-20 12:13:17 +0100

    User sign up and authentication2

--- config.py
+++ config.py
@@ -7,3 +7,8 @@ db_port = os.environ.get('DB_PORT', default='5432')
 
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 SQLALCHEMY_DATABASE_URI = f"postgresql://{db_user}:{db_password}@{db_host}:{db_port}/{db_name}"
+
+print("DEBUG: Database URI ->", SQLALCHEMY_DATABASE_URI)  # Add this line to check the URI
+
+#SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL', 'postgresql://demo:secure-password@54.170.97.218/sample')
+#SQLALCHEMY_TRACK_MODIFICATIONS = False
